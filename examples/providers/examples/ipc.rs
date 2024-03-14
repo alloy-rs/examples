2c055ae93410d1eb878d02186d6ee8296f353316
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-03-14 14:32:39 -0400

    feat(providers): ipc transport

---
+++ examples/providers/examples/ipc.rs
@@ -0,0 +1,25 @@
+use alloy_network::Ethereum;
+use alloy_provider::{Provider, RootProvider};
+use alloy_rpc_client::RpcClient;
+use alloy_transport_ipc::IpcConnect;
+use eyre::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Setup the IPC transport which is consumed by the RPC client
+    let ipc_path = "/tmp/reth.ipc";
+
+    // IPC transport
+    let ipc = IpcConnect::from(ipc_path.to_string());
+
+    // RPC client using IPC transport
+    let ipc_client = RpcClient::connect_pubsub(ipc).await?;
+
+    let provider = RootProvider::<Ethereum, _>::new(ipc_client);
+
+    let latest_block = provider.get_block_number().await?;
+
+    println!("Latest block: {}", latest_block);
+
+    Ok(())
+}
