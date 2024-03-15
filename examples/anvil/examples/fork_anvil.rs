c96ec82cb555221086dddcb408b9da94e9be33b3
zerosnacks <zerosnacks@protonmail.com>
2024-03-15 10:03:54 +0000

    add eyre and update examples

--- examples/anvil/examples/fork_anvil.rs
+++ examples/anvil/examples/fork_anvil.rs
@@ -1,9 +1,10 @@
 //! Example of spinning up a forked Anvil node.
 
 use alloy_node_bindings::Anvil;
+use eyre::Result;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     // Spin up a forked Anvil node.
     // Ensure `anvil` is available in $PATH
     let anvil = Anvil::new().fork("https://eth.llamarpc.com").spawn();
--- examples/anvil/examples/local_anvil.rs
+++ examples/anvil/examples/local_anvil.rs
@@ -1,9 +1,10 @@
 //! Example of spinning up a local Anvil node.
 
 use alloy_node_bindings::Anvil;
+use eyre::Result;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     // Spin up a local Anvil node.
     // Ensure `anvil` is available in $PATH
     let anvil = Anvil::new().spawn();
