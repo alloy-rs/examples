3fbfc56e5bc9bcc37d8ca987aa8d206c0eb57cd5
zerosnacks <zerosnacks@protonmail.com>
2024-03-18 09:40:26 +0000

    clean up

--- examples/anvil/examples/deploy_contract_anvil.rs
+++ examples/anvil/examples/deploy_contract_anvil.rs
@@ -11,6 +11,23 @@ use alloy_transport_http::Http;
 use eyre::Result;
 use reqwest::Client;
 
+// Codegen from embedded Solidity code and precompiled bytecode.
+sol! {
+    // solc v0.8.24; solc a.sol --via-ir --optimize --bin
+    #[sol(rpc, bytecode="608080604052346100155760d2908161001a8239f35b5f80fdfe60808060405260043610156011575f80fd5b5f3560e01c9081633fb5c1cb1460865781638381f58a14606f575063d09de08a146039575f80fd5b34606b575f366003190112606b575f545f1981146057576001015f55005b634e487b7160e01b5f52601160045260245ffd5b5f80fd5b34606b575f366003190112606b576020905f548152f35b34606b576020366003190112606b576004355f5500fea2646970667358221220bdecd3c1dd631eb40587cafcd6e8297479db76db6a328e18ad1ea5b340852e3864736f6c63430008180033")]
+    contract Counter {
+        uint256 public number;
+
+        function setNumber(uint256 newNumber) public {
+            number = newNumber;
+        }
+
+        function increment() public {
+            number++;
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     // Spin up a local Anvil node.
@@ -27,23 +44,6 @@ async fn main() -> Result<()> {
         .provider(RootProvider::new(RpcClient::new(http, true)));
 
     println!("Anvil running at `{}`", anvil.endpoint());
-
-    // Create a contract.
-    sol! {
-        // solc v0.8.24; solc a.sol --via-ir --optimize --bin
-        #[sol(rpc, bytecode="608080604052346100155760d2908161001a8239f35b5f80fdfe60808060405260043610156011575f80fd5b5f3560e01c9081633fb5c1cb1460865781638381f58a14606f575063d09de08a146039575f80fd5b34606b575f366003190112606b575f545f1981146057576001015f55005b634e487b7160e01b5f52601160045260245ffd5b5f80fd5b34606b575f366003190112606b576020905f548152f35b34606b576020366003190112606b576004355f5500fea2646970667358221220bdecd3c1dd631eb40587cafcd6e8297479db76db6a328e18ad1ea5b340852e3864736f6c63430008180033")]
-        contract Counter {
-            uint256 public number;
-
-            function setNumber(uint256 newNumber) public {
-                number = newNumber;
-            }
-
-            function increment() public {
-                number++;
-            }
-        }
-    }
 
     // Get the base fee for the block.
     let base_fee = provider.get_gas_price().await?;
