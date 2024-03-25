1e68146d7dc0bc56aa97db84f51b8b57252125aa
zerosnacks <zerosnacks@protonmail.com>
2024-03-25 13:34:54 +0000

    chore: cleanup

--- examples/wallets/examples/create_keystore.rs
+++ examples/wallets/examples/create_keystore.rs
@@ -11,28 +11,32 @@ async fn main() -> Result<()> {
     let dir = tempdir()?;
     let mut rng = thread_rng();
 
-    // Password to encrypt the keystore file with.
-    let password = "test";
-
-    // Set up a local wallet from the first default Anvil account (Alice).
+    // Private key of Alice, the first default Anvil account.
     let private_key =
         hex::decode("ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")?;
 
-    // Create a keystore file from the private key.
-    let (key, uuid) =
+    // Password to encrypt the keystore file with.
+    let password = "test";
+
+    // Create a keystore file from the private key of Alice, returning a [Wallet] instance.
+    let (wallet, file_path) =
         Wallet::encrypt_keystore(&dir, &mut rng, private_key, password, None).unwrap();
 
-    let key_file_path = dir.path().join(uuid.clone());
+    let key_file_path = dir.path().join(file_path.clone());
 
-    println!("Wrote keystore for {:?} to {:?}", key.address(), key_file_path);
+    println!("Wrote keystore for {:?} to {:?}", wallet.address(), key_file_path);
 
     // Read the keystore file back.
-    let wallet = Wallet::decrypt_keystore(key_file_path.clone(), password)?;
+    let recovered_wallet = Wallet::decrypt_keystore(key_file_path.clone(), password)?;
 
-    println!("Read keystore from {:?}, recovered address: {:?}", key_file_path, wallet.address());
+    println!(
+        "Read keystore from {:?}, recovered address: {:?}",
+        key_file_path,
+        recovered_wallet.address()
+    );
 
     // Assert that the address of the original key and the recovered key are the same.
-    assert_eq!(wallet.address(), key.address());
+    assert_eq!(wallet.address(), recovered_wallet.address());
 
     // Display the contents of the keystore file.
     let keystore_contents = read_to_string(key_file_path)?;
