03d233089935f06c96d489b0b24c479e2768c968
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-04-22 14:49:55 +0100

    fmt

--- examples/subscriptions/examples/subscribe_to_log_external_provider.rs
+++ examples/subscriptions/examples/subscribe_to_log_external_provider.rs
@@ -1,10 +1,15 @@
 //! Example of subscribing to logs from the Ethereum network using an external provider.
 
-use alloy::{primitives::{address, b256}, providers::{Provider, ProviderBuilder}, rpc::{client::WsConnect, types::eth::{BlockNumberOrTag, Filter}}};
+use alloy::{
+    primitives::{address, b256},
+    providers::{Provider, ProviderBuilder},
+    rpc::{
+        client::WsConnect,
+        types::eth::{BlockNumberOrTag, Filter},
+    },
+};
 use eyre::Result;
 use futures_util::stream::StreamExt;
-
-
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -16,16 +21,13 @@ async fn main() -> Result<()> {
         .event_signature(tranfer_event_signature)
         .from_block(BlockNumberOrTag::Latest);
 
-    let rpc_url = "RPC_URL"; // DON'T use wss://eth.merkle.io _> this filters wrongly, tested alchemy.io's to be working fine
+    let rpc_url = "wss://eth.merkle.io"; // DON'T use wss://eth.merkle.io _> this filters wrongly, tested alchemy.io's to be working fine
 
     // Create the provider.
     let ws = WsConnect::new(rpc_url);
     let provider = ProviderBuilder::new().on_ws(ws).await.unwrap();
 
-    let sub = provider
-        .subscribe_logs(&filter)
-        .await
-        .expect("Failed to subscribe to logs");
+    let sub = provider.subscribe_logs(&filter).await.expect("Failed to subscribe to logs");
     let mut stream = sub.into_stream();
 
     while let Some(log) = stream.next().await {
