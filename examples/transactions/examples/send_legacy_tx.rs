4f8bec813c51c099d3e64004848e928b032a2724
zerosnacks <zerosnacks@protonmail.com>
2024-05-06 16:15:44 +0000

    add explicit send_(legacy|eip1559|eip4844)_tx

---
+++ examples/transactions/examples/send_legacy_tx.rs
@@ -0,0 +1,52 @@
+//! Example showing how to send a legacy transaction.
+
+use alloy::{
+    network::TransactionBuilder,
+    node_bindings::Anvil,
+    primitives::U256,
+    providers::{Provider, ProviderBuilder},
+    rpc::types::eth::TransactionRequest,
+};
+use eyre::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Spin up a local Anvil node.
+    // Ensure `anvil` is available in $PATH.
+    let anvil = Anvil::new().try_spawn()?;
+
+    // Create a provider.
+    let rpc_url = anvil.endpoint().parse()?;
+    let provider = ProviderBuilder::new().on_http(rpc_url);
+
+    // Create two users, Alice and Bob.
+    let alice = anvil.addresses()[0];
+    let bob = anvil.addresses()[1];
+
+    // Build a transaction to send 100 wei from Alice to Bob.
+    let tx = TransactionRequest::default()
+        .with_from(alice)
+        .with_to(bob)
+        .with_nonce(0)
+        .with_value(U256::from(100))
+        .with_gas_price(20_000_000_000)
+        .with_gas_limit(21_000);
+
+    // Send the transaction and wait for the receipt.
+    let pending_tx = provider.send_transaction(tx).await?;
+
+    println!("Pending transaction...{:?}", pending_tx.tx_hash());
+
+    // Wait for the transaction to be included.
+    let receipt = pending_tx.get_receipt().await?;
+
+    println!(
+        "Transaction included in block: {:?}",
+        receipt.block_number.expect("Failed to get block number").to_string()
+    );
+
+    assert_eq!(receipt.from, alice);
+    assert_eq!(receipt.to, Some(bob));
+
+    Ok(())
+}
