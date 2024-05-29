2410facc2f1f826d848b7e187a1200b3fe307274
zerosnacks <zerosnacks@protonmail.com>
2024-05-29 09:13:53 +0200

    lint fixes, comment fixes

--- examples/contracts/examples/deploy_from_bytecode.rs
+++ examples/contracts/examples/deploy_from_bytecode.rs
@@ -1,4 +1,4 @@
-//! Example of deploying a contract at Runtime from Solidity code to Anvil and interacting with it.
+//! Example of deploying a contract at runtime from Solidity code to Anvil and interacting with it.
 
 use alloy::{
     hex::FromHex,
@@ -50,6 +50,7 @@ async fn main() -> Result<()> {
         .on_http(rpc_url);
 
     // We deploy the contract at run time from bytecode
+    // solc v0.8.24; solc a.sol --via-ir --optimize --bin
     let contract_bytecode = "608080604052346100155760d2908161001a8239f35b5f80fdfe60808060405260043610156011575f80fd5b5f3560e01c9081633fb5c1cb1460865781638381f58a14606f575063d09de08a146039575f80fd5b34606b575f366003190112606b575f545f1981146057576001015f55005b634e487b7160e01b5f52601160045260245ffd5b5f80fd5b34606b575f366003190112606b576020905f548152f35b34606b576020366003190112606b576004355f5500fea2646970667358221220bdecd3c1dd631eb40587cafcd6e8297479db76db6a328e18ad1ea5b340852e3864736f6c63430008180033";
     let data: Vec<u8> = FromHex::from_hex(contract_bytecode)?;
     let tx = TransactionRequest::default().with_deploy_code(data);
