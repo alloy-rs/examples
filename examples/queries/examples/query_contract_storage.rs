f11bea87be793f5bdb17107c18ea6b4acaefd231
lavrentivs <llorenc.escoter@gmail.com>
2024-07-11 15:13:00 +0200

    Typo query_contract_storage.rs

--- examples/queries/examples/query_contract_storage.rs
+++ examples/queries/examples/query_contract_storage.rs
@@ -15,7 +15,7 @@ async fn main() -> Result<()> {
     // Get storage slot 0 from the Uniswap V3 USDC-ETH pool on Ethereum mainnet.
     let pool_address = address!("88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640");
     let storage_slot = U256::from(0);
-    // The provider calls the RPC at the latest block by default. A block can exlpicitly be set
+    // The provider calls the RPC at the latest block by default. A block can explicitly be set
     // using `.block()`.
     let storage = provider.get_storage_at(pool_address, storage_slot).await?;
 
