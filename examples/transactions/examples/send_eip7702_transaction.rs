26c73b663ab1e3073a32a3ec1c49faa3d6d25f41
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-08-15 21:55:26 -0700

    feat(transactions): send_7702_tx

---
+++ examples/transactions/examples/send_eip7702_transaction.rs
@@ -0,0 +1,89 @@
+//! This example demonstrates how to send an EIP7702 transaction.
+use alloy::{
+    consensus::{SignableTransaction, TxEip7702},
+    eips::eip7702::Authorization,
+    network::TxSignerSync,
+    node_bindings::Anvil,
+    primitives::{TxKind, U256},
+    providers::{Provider, ProviderBuilder},
+    signers::{local::LocalSigner, SignerSync},
+    sol,
+};
+use eyre::Result;
+
+// Codegen from embedded Solidity code and precompiled bytecode.
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc, bytecode = "6080604052348015600f57600080fd5b5060d980601d6000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c80637b3ab2d01460375780639ee1a44014603f575b600080fd5b603d6047565b005b60456075565b005b7fbcdfe0d5b27dd186282e187525415c57ea3077c34efb39148111e4d342e7ab0e60405160405180910390a1565b7f2d67bb91f17bca05af6764ab411e86f4ddf757adb89fcec59a7d21c525d4171260405160405180910390a156fea2646970667358221220eff0a4721c5625c372d3b4a7e348d36d5ee4dd32a26abdd94cba7839b45852d764736f6c63430008190033")]
+    contract Log {
+        #[derive(Debug)]
+        event Hello();
+        event World();
+
+        function emitHello() public {
+            emit Hello();
+        }
+
+        function emitWorld() public {
+            emit World();
+        }
+    }
+);
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let anvil = Anvil::new().arg("--hardfork").arg("prague").try_spawn()?;
+
+    let authority = LocalSigner::from_signing_key(anvil.keys()[0].clone().into()); // 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
+    let sender = LocalSigner::from_signing_key(anvil.keys()[1].clone().into());
+    let provider = ProviderBuilder::new().on_http(anvil.endpoint_url());
+
+    let contract = Log::deploy(&provider).await?;
+
+    println!("Deployed contract address: {}", contract.address());
+    println!("Setting code for address: {}", authority.address());
+
+    let auth_7702 = Authorization {
+        chain_id: U256::from(31337),
+        address: *contract.address(), /* Reference to the contract that will be set as code for
+                                       * the authority */
+        nonce: provider.get_transaction_count(authority.address()).await?,
+    };
+
+    // Sign the authorization
+    let sig = authority.sign_hash_sync(&auth_7702.signature_hash())?;
+    let auth = auth_7702.into_signed(sig);
+
+    // Collect the calldata required for the tx
+    let call = contract.emitHello();
+    let emit_hello_calldata = call.calldata().to_owned();
+
+    // Estimate the EIP1559 fees
+    let eip1559_est = provider.estimate_eip1559_fees(None).await?;
+
+    // Build the transaction
+    let mut tx = TxEip7702 {
+        to: TxKind::Call(authority.address()),
+        authorization_list: vec![auth],
+        input: emit_hello_calldata.to_owned(),
+        nonce: provider.get_transaction_count(sender.address()).await?,
+        chain_id: 31337,
+        gas_limit: 1000000,
+        max_fee_per_gas: eip1559_est.max_fee_per_gas,
+        max_priority_fee_per_gas: eip1559_est.max_priority_fee_per_gas,
+        ..Default::default()
+    };
+
+    // Sign and Encode the transaction
+    let sig = sender.sign_transaction_sync(&mut tx)?;
+    let tx = tx.into_signed(sig);
+    let mut encoded = Vec::new();
+    tx.tx().encode_with_signature(tx.signature(), &mut encoded, false);
+    let receipt = provider.send_raw_transaction(&encoded).await?.get_receipt().await?;
+
+    assert!(receipt.status());
+    assert!(receipt.inner.logs().len() == 1);
+    assert!(receipt.inner.logs()[0].address() == authority.address());
+
+    Ok(())
+}
