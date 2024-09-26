6f6c6d5c8edad4a14deff86b14311bb6a798a344
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-09-26 12:06:33 +0530

    docs

--- examples/advanced/examples/reth_db_provider.rs
+++ examples/advanced/examples/reth_db_provider.rs
@@ -1,4 +1,15 @@
-//! Demonstrates how to leverage `ProviderCall` to wrap the `Provider` trait over reth-db.
+//! `ProviderCall` enables the alloy-provider to fetch results of a rpc request from arbitrary
+//! sources. These arbitray sources could be a RPC call over the network, a local database, or even
+//! a synchronous function call.
+//!
+//! `ProviderCall` is the final future in the flow of an rpc request and is used by the
+//! `RpcWithBlock` and `EthCall` types under the hood to give flexibility to the user to use
+//! their own implementation of the `Provider` trait and fetch results from any source.
+//!
+//! In this minimal example, we demonstrate how we wrap the `Provider` trait over reth-db and
+//! leverage `ProviderCall`.
+//!
+//! Learn more about `ProviderCall` [here](https://github.com/alloy-rs/alloy/pull/788).
 use std::{marker::PhantomData, path::PathBuf, sync::Arc};
 
 use alloy::{
@@ -29,6 +40,7 @@ use reth_provider::{
 #[tokio::main]
 async fn main() -> Result<()> {
     run_with_tempdir("provider-call-reth-db", |data_dir| async move {
+        // Initialize reth node with a specific data directory
         let reth = Reth::new()
             .dev()
             .disable_discovery()
@@ -38,53 +50,63 @@ async fn main() -> Result<()> {
 
         let db_path = data_dir.join("db");
 
+        // Initialize the provider with the reth-db layer. The reth-db layer intercepts the rpc
+        // requests and returns the results from the reth-db database.
+        // Any RPC method that is not implemented in the RethDBProvider gracefully falls back to the
+        // RPC provider specified in the `on_http` method.
         let provider =
             ProviderBuilder::new().layer(RethDBLayer::new(db_path)).on_http(reth.endpoint_url());
 
+        // Initialize the RPC provider to compare the time taken to fetch the results.
         let rpc_provider = ProviderBuilder::new().on_http(reth.endpoint_url());
 
-        let start_t = std::time::Instant::now();
-        let latest_block = provider.get_block_number().await.unwrap();
-        println!("Latest block from DB={latest_block} | Time Taken: {:?}", start_t.elapsed());
+        println!("--------get_block_number---------");
 
         let start_t = std::time::Instant::now();
-        let latest_block = rpc_provider.get_block_number().await.unwrap();
-        println!("Latest block from RPC={latest_block} | Time Taken: {:?}", start_t.elapsed());
+        let latest_block_db = provider.get_block_number().await.unwrap();
+        println!("via reth-db: {:?}", start_t.elapsed());
+
+        let start_t = std::time::Instant::now();
+        let latest_block_rpc = rpc_provider.get_block_number().await.unwrap();
+        println!("via rpc:     {:?}\n", start_t.elapsed());
+
+        assert_eq!(latest_block_db, latest_block_rpc);
+
+        println!("------get_transaction_count------");
 
         let alice = address!("14dC79964da2C08b23698B3D3cc7Ca32193d9955");
 
         let start_t = std::time::Instant::now();
-        let nonce = provider.get_transaction_count(alice).await.unwrap();
-        println!("Nonce from DB={nonce} | Time Taken: {:?}", start_t.elapsed());
+        let nonce_db =
+            provider.get_transaction_count(alice).block_id(BlockId::latest()).await.unwrap();
+        println!("via reth-db: {:?}", start_t.elapsed());
 
         let start_t = std::time::Instant::now();
-        let nonce = rpc_provider.get_transaction_count(alice).await.unwrap();
-        println!("Nonce from RPC={nonce} | Time Taken: {:?}", start_t.elapsed());
+        let nonce_rpc =
+            rpc_provider.get_transaction_count(alice).block_id(BlockId::latest()).await.unwrap();
+        println!("via rpc:     {:?}\n", start_t.elapsed());
 
-        let nonce_at_block = provider
-            .get_transaction_count(alice)
-            .block_id(BlockId::Number(BlockNumberOrTag::Number(1)))
-            .await
-            .unwrap();
-        println!("Nonce from DB at block 1={nonce_at_block}");
+        assert_eq!(nonce_db, nonce_rpc);
     })
     .await;
 
     Ok(())
 }
 
-/// A `ProviderLayer` that wraps the `Provider` trait over reth-db.
+/// We use the tower-like layering functionality that has been baked into the alloy-provider to
+/// intercept the requests and redirect to the `RethDBProvider`.
 struct RethDBLayer {
     db_path: PathBuf,
 }
 
-/// Implement the `ProviderLayer` trait for `RethDBLayer`.
+/// Initialize the `RethDBLayer` with the path to the reth datadir.
 impl RethDBLayer {
     const fn new(db_path: PathBuf) -> Self {
         Self { db_path }
     }
 }
 
+/// Implement the `ProviderLayer` trait for the `RethDBLayer` struct.
 impl<P, T> ProviderLayer<P, T> for RethDBLayer
 where
     P: Provider<T>,
@@ -97,7 +119,10 @@ where
     }
 }
 
-/// A provider that wraps the `Provider` trait over reth-db.
+/// A provider that overrides the vanilla `Provider` trait to get results from the reth-db.
+///
+/// It holds the `reth_provider::ProviderFactory` that enables read-only access to the database
+/// tables and static files.
 #[derive(Clone, Debug)]
 pub struct RethDBProvider<P, T> {
     inner: P,
@@ -135,6 +160,9 @@ impl<P, T> RethDBProvider<P, T> {
     }
 }
 
+/// Implement the `Provider` trait for the `RethDBProvider` struct.
+///
+/// This is where we override specific RPC methods to fetch from the reth-db.
 impl<P, T> Provider<T> for RethDBProvider<P, T>
 where
     P: Provider<T>,
@@ -144,6 +172,7 @@ where
         self.inner.root()
     }
 
+    /// Override the `get_block_number` method to fetch the latest block number from the reth-db.
     fn get_block_number(&self) -> ProviderCall<T, NoParams, U64, u64> {
         let provider = self.factory().provider().map_err(TransportErrorKind::custom).unwrap();
 
@@ -154,6 +183,9 @@ where
         ProviderCall::<T, NoParams, U64, u64>::ready(best)
     }
 
+    /// Override the `get_transaction_count` method to fetch the transaction count of an address.
+    ///
+    /// `RpcWithBlock` uses `ProviderCall` under the hood.
     fn get_transaction_count(&self, address: Address) -> RpcWithBlock<T, Address, U64, u64> {
         let this = self.factory().clone();
         RpcWithBlock::new_provider(move |block_id| {
@@ -171,6 +203,7 @@ where
     }
 }
 
+/// A helper type to get the appropriate DB provider from `reth_provider::ProviderFactory`.
 #[derive(Clone, Debug)]
 struct WrapProviderFactory {
     inner: Arc<ProviderFactory<NodeTypesWithDBAdapter<EthereumNode, Arc<DatabaseEnv>>>>,
@@ -183,11 +216,12 @@ impl WrapProviderFactory {
         Self { inner }
     }
 
-    /// Get the DB provider.
+    /// Get a read-only `DatabaseProvider`
     fn provider(&self) -> Result<DatabaseProvider<Tx<RO>, ChainSpec>, ProviderError> {
         self.inner.provider()
     }
 
+    /// Get a read-only `DatabaseProvider` at a specific block
     fn provider_at(
         &self,
         block_id: BlockId,
