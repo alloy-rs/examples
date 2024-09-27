33dd38eaac58a3119e3d4532b10d78f0b9ef0a96
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-09-27 12:51:56 +0530

    transact with revm

--- examples/advanced/examples/foundry_fork_db.rs
+++ examples/advanced/examples/foundry_fork_db.rs
@@ -5,15 +5,18 @@ use eyre::Result;
 
 use alloy::{
     eips::BlockId,
-    network::AnyNetwork,
+    network::{AnyNetwork, TransactionBuilder},
     node_bindings::Anvil,
+    primitives::U256,
     providers::{Provider, ProviderBuilder},
-    rpc::types::BlockTransactionsKind,
+    rpc::types::{
+        serde_helpers::WithOtherFields, Block, BlockTransactionsKind, Transaction,
+        TransactionRequest,
+    },
 };
 use foundry_fork_db::{cache::BlockchainDbMeta, BlockchainDb, SharedBackend};
-
-// TODO: Add docs and explanation of the workflow.
-// TODO: Depict how the backend handler can smartly manages duplicate requests.
+use revm::{db::CacheDB, DatabaseRef, Evm};
+use revm_primitives::{BlobExcessGasAndPrice, BlockEnv, TxEnv};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -32,13 +35,14 @@ async fn main() -> Result<()> {
 
     let db = BlockchainDb::new(meta, None);
 
-    let shared = SharedBackend::spawn_backend(Arc::new(provider), db, Some(pin_block)).await;
+    let shared =
+        SharedBackend::spawn_backend(Arc::new(provider.clone()), db, Some(pin_block)).await;
 
     let start_t = std::time::Instant::now();
     let block_rpc = shared.get_full_block(0).unwrap();
     let time_rpc = start_t.elapsed();
 
-    // `SharedBackend` are clonable and have the same underlying cache.
+    // `SharedBackend` is cloneable and holds the channel to the same `BackendHandler`.
     let cloned_backend = shared.clone();
 
     // Block gets cached in the db
@@ -50,8 +54,89 @@ async fn main() -> Result<()> {
 
     assert!(time_cache < time_rpc);
 
-    println!("Time taken for 1st request  (via RPC): {:?}", time_rpc);
-    println!("Time taken for 2nd requst (via Cache): {:?}", time_cache);
+    println!("-------get_full_block--------");
+    // The backend handle falls back to the RPC provider if the block is not in the cache.
+    println!("1st request     (via rpc): {:?}", time_rpc);
+    // The block is cached due to the previous request and can be fetched from db.
+    println!("2nd request (via fork db): {:?}\n", time_cache);
+
+    let alice = anvil.addresses()[0];
+    let bob = anvil.addresses()[1];
+
+    let basefee = block.header.base_fee_per_gas.unwrap();
+
+    let tx_req = TransactionRequest::default()
+        .with_from(alice)
+        .with_to(bob)
+        .with_value(U256::from(100))
+        .with_max_fee_per_gas(basefee)
+        .with_max_priority_fee_per_gas(basefee + 1)
+        .with_gas_limit(21000)
+        .with_nonce(0);
+
+    let mut evm = configure_evm_env(block, shared.clone(), configure_tx_env(tx_req));
+
+    // Fetches accounts from the RPC
+    let start_t = std::time::Instant::now();
+    let alice_bal = shared.basic_ref(alice)?.unwrap().balance;
+    let bob_bal = shared.basic_ref(bob)?.unwrap().balance;
+    let time_rpc = start_t.elapsed();
+
+    let res = evm.transact().unwrap();
+
+    let total_spent = U256::from(res.result.gas_used()) * U256::from(basefee) + U256::from(100);
+
+    shared.data().do_commit(res.state);
+
+    // Fetches accounts from the cache
+    let start_t = std::time::Instant::now();
+    let alice_bal_after = shared.basic_ref(alice)?.unwrap().balance;
+    let bob_bal_after = shared.basic_ref(bob)?.unwrap().balance;
+    let time_cache = start_t.elapsed();
+
+    println!("-------get_account--------");
+    println!("1st request     (via rpc): {:?}", time_rpc);
+    println!("2nd request (via fork db): {:?}\n", time_cache);
+
+    assert_eq!(alice_bal_after, alice_bal - total_spent);
+    assert_eq!(bob_bal_after, bob_bal + U256::from(100));
 
     Ok(())
+}
+
+fn configure_evm_env(
+    block: WithOtherFields<Block<WithOtherFields<Transaction>>>,
+    shared: SharedBackend,
+    tx_env: TxEnv,
+) -> Evm<'static, (), CacheDB<SharedBackend>> {
+    let basefee = block.header.base_fee_per_gas.map(U256::from).unwrap_or_default();
+    let block_env = BlockEnv {
+        number: U256::from(block.header.number),
+        coinbase: block.header.miner,
+        timestamp: U256::from(block.header.timestamp),
+        gas_limit: U256::from(block.header.gas_limit),
+        basefee,
+        prevrandao: block.header.mix_hash,
+        difficulty: block.header.difficulty,
+        blob_excess_gas_and_price: Some(BlobExcessGasAndPrice::new(
+            block.header.excess_blob_gas.map(|g| g as u64).unwrap_or_default(),
+        )),
+    };
+
+    let db = CacheDB::new(shared);
+
+    let evm = Evm::builder().with_block_env(block_env).with_db(db).with_tx_env(tx_env).build();
+
+    evm
+}
+
+fn configure_tx_env(tx_req: TransactionRequest) -> TxEnv {
+    TxEnv {
+        caller: tx_req.from.unwrap(),
+        transact_to: tx_req.to.unwrap(),
+        value: tx_req.value.unwrap(),
+        gas_price: U256::from(tx_req.max_fee_per_gas.unwrap()),
+        gas_limit: tx_req.gas.map(|g| g as u64).unwrap_or_default(),
+        ..Default::default()
+    }
 }
