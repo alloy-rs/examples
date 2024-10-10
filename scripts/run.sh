227f0d9522830ed39a2fce2a965b744c4341049a
zerosnacks <95942363+zerosnacks@users.noreply.github.com>
2024-10-10 11:55:01 +0200

    fix(ci): integration, simplify maintenance of runnable examples list (#151)
    * prebuild prior to running
    
    * bump to 0.4.1
    
    * avoid pre-building
    
    * use `run.sh` script to gather examples list and run in tests and integration test
    
    * fix indentation
    
    * clean up
    
    * ignore version tag, fix integration test

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,38 +1,50 @@
 #!/usr/bin/env bash
 
-# Exit if anything fails
+# Exit if anything fails.
 set -eo pipefail
 
 # This script will do the following:
 #
-# 1. Run all examples with some exceptions.
+# 1. Gather all the examples from the output of `cargo run --example` command.
+# 2. Filter out the examples that have external dependencies or are not meant to be run.
+# 1. Run all examples that are left after filtering.
 function main () {
-    cargo run --example 2>&1 \
-    | grep -E '^ ' \
-    | grep -v \
-    -e 'any_network' \
-    -e 'builtin' \
-    -e 'geth_local_instance' \
-    -e 'ipc' \
-    -e 'ledger_signer' \
-    -e 'reth_local_instance' \
-    -e 'subscribe_all_logs' \
-    -e 'subscribe_logs' \
-    -e 'subscribe_pending_transactions' \
-    -e 'trace_call' \
-    -e 'trace_transaction' \
-    -e 'trezor_signer' \
-    -e 'ws_auth' \
-    -e 'ws' \
-    -e 'yubi_signer' \
-    | xargs -I {} sh -c 'if cargo run --example {} --quiet 1>/dev/null; then \
-            echo "Successfully ran: {}"; \
-        else \
-            echo "Failed to run: {}"; \
-            cargo run --example {}; \
-        fi'
+    export examples="$(
+        cargo run --example 2>&1 \
+            | grep -E '^ ' \
+            | grep -v \
+            -e 'any_network' \
+            -e 'builtin' \
+            -e 'geth_local_instance' \
+            -e 'ipc' \
+            -e 'ledger_signer' \
+            -e 'reth_db_layer' \
+            -e 'reth_db_provider' \
+            -e 'reth_local_instance' \
+            -e 'subscribe_all_logs' \
+            -e 'subscribe_logs' \
+            -e 'subscribe_pending_transactions' \
+            -e 'trace_call' \
+            -e 'trace_transaction' \
+            -e 'trezor_signer' \
+            -e 'ws_auth' \
+            -e 'ws' \
+            -e 'yubi_signer' \
+            | xargs -n1 echo
+    )"
+
+    for example in $examples; do
+        cargo run --example $example --quiet 1>/dev/null
+
+        if [ $? -ne 0 ]; then
+            echo "Failed to run: $example"
+            exit 1
+        else
+            echo "Successfully ran: $example"
+        fi
+    done
 }
 
-# Run the main function
-# This prevents partial execution in case of incomplete downloads
+# Run the main function.
+# This prevents partial execution in case of incomplete downloads.
 main
