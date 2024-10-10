6eebe284f8e23fee7eade2b9c944f1ec9cfe4641
zerosnacks <zerosnacks@protonmail.com>
2024-10-10 11:36:12 +0200

    fix indentation

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,38 +9,38 @@ set -eo pipefail
 function main () {
     export examples="$(
         cargo run --example 2>&1 \
-        | grep -E '^ ' \
-        | grep -v \
-        -e 'any_network' \
-        -e 'builtin' \
-        -e 'geth_local_instance' \
-        -e 'ipc' \
-        -e 'ledger_signer' \
-        -e 'reth_db_layer' \
-        -e 'reth_db_provider' \
-        -e 'reth_local_instance' \
-        -e 'subscribe_all_logs' \
-        -e 'subscribe_logs' \
-        -e 'subscribe_pending_transactions' \
-        -e 'trace_call' \
-        -e 'trace_transaction' \
-        -e 'trezor_signer' \
-        -e 'ws_auth' \
-        -e 'ws' \
-        -e 'yubi_signer' \
-        | xargs -n1 echo
+            | grep -E '^ ' \
+            | grep -v \
+            -e 'any_network' \
+            -e 'builtin' \
+            -e 'geth_local_instance' \
+            -e 'ipc' \
+            -e 'ledger_signer' \
+            -e 'reth_db_layer' \
+            -e 'reth_db_provider' \
+            -e 'reth_local_instance' \
+            -e 'subscribe_all_logs' \
+            -e 'subscribe_logs' \
+            -e 'subscribe_pending_transactions' \
+            -e 'trace_call' \
+            -e 'trace_transaction' \
+            -e 'trezor_signer' \
+            -e 'ws_auth' \
+            -e 'ws' \
+            -e 'yubi_signer' \
+            | xargs -n1 echo
     )"
 
     # Run the examples with the current version of Alloy
     for example in $examples; do
-    cargo run --example $example --quiet 1>/dev/null
+        cargo run --example $example --quiet 1>/dev/null
 
-    if [ $? -ne 0 ]; then
-        echo "Failed to run: $example"
-        exit 1
-    else
-        echo "Successfully ran: $example"
-    fi
+        if [ $? -ne 0 ]; then
+            echo "Failed to run: $example"
+            exit 1
+        else
+            echo "Successfully ran: $example"
+        fi
     done
 }
 
