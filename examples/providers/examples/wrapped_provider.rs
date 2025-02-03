b84ec69c7f5de0351db2ccb4436d0e862b428ddd
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-02-03 17:12:06 +0530

    fix

--- examples/providers/examples/wrapped_provider.rs
+++ examples/providers/examples/wrapped_provider.rs
@@ -7,19 +7,37 @@ use alloy::{
     providers::{Provider, ProviderBuilder},
     signers::local::PrivateKeySigner,
     sol,
-    transports::{BoxTransport, TransportResult},
+    transports::TransportResult,
 };
 use eyre::Result;
 use Counter::CounterInstance;
 
 /// Creates and returns an implementation of the [`Provider`] trait.
 async fn get_provider(url: &str) -> TransportResult<impl Provider> {
-    ProviderBuilder::new().with_recommended_fillers().on_builtin(url).await
+    ProviderBuilder::new().on_builtin(url).await
 }
 
 /// Simple free function to get the latest block number.
 async fn get_block_number<P: Provider>(provider: &P) -> TransportResult<u64> {
     provider.get_block_number().await
+}
+
+// Codegen from embedded Solidity code and precompiled bytecode.
+sol! {
+    #[allow(missing_docs)]
+    // solc v0.8.26; solc Counter.sol --via-ir --optimize --bin
+    #[sol(rpc, bytecode="6080806040523460135760df908160198239f35b600080fdfe6080806040526004361015601257600080fd5b60003560e01c9081633fb5c1cb1460925781638381f58a146079575063d09de08a14603c57600080fd5b3460745760003660031901126074576000546000198114605e57600101600055005b634e487b7160e01b600052601160045260246000fd5b600080fd5b3460745760003660031901126074576020906000548152f35b34607457602036600319011260745760043560005500fea2646970667358221220e978270883b7baed10810c4079c941512e93a7ba1cd1108c781d4bc738d9090564736f6c634300081a0033")]
+    contract Counter {
+        uint256 public number;
+
+        function setNumber(uint256 newNumber) public {
+            number = newNumber;
+        }
+
+        function increment() public {
+            number++;
+        }
+    }
 }
 
 /// Deployer that ingests a [`Provider`] and [`EthereumWallet`] and deploys [`Counter`]
@@ -36,7 +54,7 @@ impl<P: Provider> Deployer<P> {
     }
 
     /// Deploys [`Counter`] using the given [`EthereumWallet`] and returns [`CounterInstance`]
-    async fn deploy(&self) -> Result<CounterInstance<BoxTransport, &P>> {
+    async fn deploy(&self) -> Result<CounterInstance<(), &P>> {
         let addr = CounterInstance::deploy_builder(&self.provider)
             .from(self.wallet.default_signer().address())
             .deploy()
@@ -66,22 +84,4 @@ async fn main() -> Result<()> {
     println!("Current Number {num}");
 
     Ok(())
-}
-
-// Codegen from embedded Solidity code and precompiled bytecode.
-sol! {
-    #[allow(missing_docs)]
-    // solc v0.8.26; solc Counter.sol --via-ir --optimize --bin
-    #[sol(rpc, bytecode="6080806040523460135760df908160198239f35b600080fdfe6080806040526004361015601257600080fd5b60003560e01c9081633fb5c1cb1460925781638381f58a146079575063d09de08a14603c57600080fd5b3460745760003660031901126074576000546000198114605e57600101600055005b634e487b7160e01b600052601160045260246000fd5b600080fd5b3460745760003660031901126074576020906000548152f35b34607457602036600319011260745760043560005500fea2646970667358221220e978270883b7baed10810c4079c941512e93a7ba1cd1108c781d4bc738d9090564736f6c634300081a0033")]
-    contract Counter {
-        uint256 public number;
-
-        function setNumber(uint256 newNumber) public {
-            number = newNumber;
-        }
-
-        function increment() public {
-            number++;
-        }
-    }
 }
