3bb98c553d82d12cb53d9a65077c2a13728c3ea0
nicolas <48695862+merklefruit@users.noreply.github.com>
2025-03-01 19:09:26 +0100

    feat: add simple fallback layer example

---
+++ examples/layers/examples/fallback_layer.rs
@@ -0,0 +1,54 @@
+//! Test the fallback layer provider.
+
+use std::{num::NonZeroUsize, time::Duration};
+
+use alloy::{
+    providers::{Provider, ProviderBuilder},
+    rpc::client::RpcClient,
+    transports::{
+        http::{reqwest::Url, Http},
+        layers::FallbackLayer,
+    },
+};
+use eyre::Result;
+use tower::ServiceBuilder;
+
+/// Note: should be run with `RUST_LOG=alloy_transport=debug` to see the rankings.
+#[tokio::main]
+async fn main() -> Result<()> {
+    let _ = tracing_subscriber::fmt::try_init();
+
+    // Configure the fallback layer
+    let fallback_layer = FallbackLayer::default()
+        .with_active_transport_count(NonZeroUsize::new(3).unwrap())
+        .with_log_transport_rankings(true);
+
+    // Define your list of transports to use
+    let transports = vec![
+        Http::new(Url::parse("https://eth.merkle.io")?),
+        Http::new(Url::parse("https://eth.llamarpc.com")?),
+        Http::new(Url::parse("https://ethereum-rpc.publicnode.com")?),
+    ];
+
+    // Apply the FallbackLayer to the transports
+    let transport = ServiceBuilder::new().layer(fallback_layer).service(transports);
+    let client = RpcClient::builder().transport(transport, false);
+    let provider = ProviderBuilder::new().on_client(client);
+
+    // Get the latest block number using the provider with ranked transports.
+    // This will also print the rankings of the transports to the console.
+    let max = 10;
+    let mut count = 0;
+    loop {
+        let latest_block = provider.get_block_number().await?;
+        println!("Latest block number: {latest_block}");
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        count += 1;
+        if count >= max {
+            break;
+        }
+    }
+
+    Ok(())
+}
