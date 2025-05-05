6a0e412083df1cf3126529daab189a0532b06f44
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-05-05 13:38:28 +0530

    refac: rlp bench

--- benches/benches/rlp.rs
+++ benches/benches/rlp.rs
@@ -11,14 +11,21 @@ pub struct MyStruct {
     pub b: Vec<u8>,
 }
 
-fn rlp_encode(c: &mut Criterion) {
-    let mut g = c.benchmark_group("rlp_encode");
+fn rlp(c: &mut Criterion) {
+    let mut g = c.benchmark_group("Rlp Encoding and Decoding");
     g.warm_up_time(std::time::Duration::from_secs(3));
 
     let my_struct = MyStruct { a: 42, b: vec![1, 2, 3, 4, 5] };
 
+    // Parity RLP encoding
+    g.bench_with_input("Parity-Rlp/Encoding", &my_struct, |b, my_struct| {
+        b.iter(|| {
+            let out = my_struct.rlp_bytes();
+            black_box(out);
+        })
+    });
     // Alloy RLP encoding
-    g.bench_with_input("rlp_encode/alloy", &my_struct, |b, my_struct| {
+    g.bench_with_input("Alloy-Rlp/Encoding", &my_struct, |b, my_struct| {
         b.iter(|| {
             let mut out = Vec::new();
             let _ = my_struct.encode(&mut out);
@@ -26,39 +33,25 @@ fn rlp_encode(c: &mut Criterion) {
         })
     });
 
-    // Parity RLP encoding
-    g.bench_with_input("rlp_encode/parity", &my_struct, |b, my_struct| {
-        b.iter(|| {
-            let out = my_struct.rlp_bytes();
-            black_box(out);
-        })
-    });
-}
-
-fn rlp_decode(c: &mut Criterion) {
-    let mut g = c.benchmark_group("rlp_decode");
-    g.warm_up_time(std::time::Duration::from_secs(3));
-
-    let my_struct = MyStruct { a: 42, b: vec![1, 2, 3, 4, 5] };
     let mut encoded = Vec::new();
     let _ = my_struct.encode(&mut encoded);
 
-    // Alloy RLP decoding
-    g.bench_with_input("rlp_decode/alloy", &encoded, |b, encoded| {
-        b.iter(|| {
-            let decoded = MyStruct::decode(&mut encoded.as_slice()).unwrap();
-            black_box(decoded);
-        })
-    });
-
     // Parity RLP decoding
-    g.bench_with_input("rlp_decode/parity", &encoded, |b, encoded| {
+    g.bench_with_input("Parity-Rlp/Decoding", &encoded, |b, encoded| {
         b.iter(|| {
             let decoded: MyStruct = rlp::decode(encoded).unwrap();
             black_box(decoded);
         })
     });
+
+    // Alloy RLP decoding
+    g.bench_with_input("Alloy-Rlp/Decoding", &encoded, |b, encoded| {
+        b.iter(|| {
+            let decoded = MyStruct::decode(&mut encoded.as_slice()).unwrap();
+            black_box(decoded);
+        })
+    });
 }
 
-criterion_group!(benches, rlp_encode, rlp_decode);
+criterion_group!(benches, rlp);
 criterion_main!(benches);
