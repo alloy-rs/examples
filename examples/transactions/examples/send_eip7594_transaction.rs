560d4a8865d0612f65d81bd7f3f2c9c3edf2e324
frankudoags <frankudoags.com>
2025-10-21 12:03:43 +0100

    wip

--- examples/transactions/examples/send_eip7594_transaction.rs
+++ examples/transactions/examples/send_eip7594_transaction.rs
@@ -1,7 +1,11 @@
 //! Example showing how to send an [EIP-7594](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-7594.md) transaction.
 
 use alloy::{
-    consensus::{SidecarBuilder, SimpleCoder},
+    consensus::{
+        EthereumTxEnvelope, SidecarBuilder, Signed, SimpleCoder, TxEip4844Variant,
+        TxEip4844WithSidecar,
+    },
+    eips::Encodable2718,
     network::{TransactionBuilder, TransactionBuilder4844},
     providers::{Provider, ProviderBuilder},
     rpc::types::TransactionRequest,
@@ -22,17 +26,46 @@ async fn main() -> Result<()> {
 
     // Create a sidecar with some data.
     let sidecar: SidecarBuilder<SimpleCoder> = SidecarBuilder::from_slice(b"Blobs are fun!");
-    let sidecar = sidecar.build_7594()?;
+    let sidecar = sidecar.build()?;
 
-    // TODO: Build a transaction to send the sidecar from Alice to Bob. I'm not sure how to create this tx atm, researching...
+    // Build a transaction to send the sidecar from Alice to Bob.
+    // The `from` field is automatically filled to the first signer's address (Alice).
+    let tx = TransactionRequest::default().with_to(bob).with_blob_sidecar(sidecar);
 
-    // Send the transaction and wait for the broadcast.
-    let pending_tx = provider.send_raw_transaction(tx).await?;
+    // Fill the transaction (e.g., nonce, gas, etc.) using the provider and convert it to an envelope.
+    let envelope = provider.fill(tx).await?.try_into_envelope()?;
+
+    // Create an EIP-4844 transaction with sidecar, using the data from the envelope(extracting the tx and sidecar).
+    let tx_with_sidecar = TxEip4844WithSidecar::from_tx_and_sidecar(
+        envelope.as_eip4844().unwrap().tx().tx().clone(),
+        envelope.as_eip4844().unwrap().tx().sidecar().unwrap().clone(),
+    );
+
+    // Here we now convert the sidecar of the transaction into the EIP-7594 variant.
+    // I want to use the conversion helpers added here: https://github.com/alloy-rs/alloy/pull/3040
+
+    // Wrap the transaction in the EIP-4844 variant enum.
+    let tx = TxEip4844Variant::TxEip4844WithSidecar(tx_with_sidecar);
+
+    // Now we encode the final transaction with the signature from the envelope.
+    let encoded_tx =
+        EthereumTxEnvelope::Eip4844(Signed::new_unhashed(tx, *envelope.signature())).encoded_2718();
+
+    // Send the raw transaction to the network.
+    let pending_tx = provider.send_raw_transaction(&encoded_tx).await?;
 
     println!("Pending transaction... {}", pending_tx.tx_hash());
 
-    // Wait for the transaction to be included and get the receipt.
+    // // Wait for the transaction to be included and get the receipt.
     let receipt = pending_tx.get_receipt().await?;
+
+    println!(
+        "Transaction included in block {}",
+        receipt.block_number.expect("Failed to get block number")
+    );
+
+    assert_eq!(receipt.from, alice);
+    assert_eq!(receipt.to, Some(bob));
 
     println!(
         "Transaction included in block {}",
