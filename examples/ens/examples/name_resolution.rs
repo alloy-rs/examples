ffaefc2d8f89b3c947e862fcf6898780044e96f5
zerosnacks <95942363+zerosnacks@users.noreply.github.com>
2025-11-03 10:25:42 +0100

    Update name_resolution.rs

--- examples/ens/examples/name_resolution.rs
+++ examples/ens/examples/name_resolution.rs
@@ -1,7 +1,6 @@
 //! Example of resolving ENS names to Ethereum addresses.
 
-use alloy::providers::ProviderBuilder;
-use alloy_ens::ProviderEnsExt;
+use alloy::{ens::ProviderEnsExt, providers::ProviderBuilder};
 use eyre::Result;
 
 #[tokio::main]
