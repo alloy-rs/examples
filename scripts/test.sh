fe4f5d2c95807a514800cdc5af6270cf1cef1c08
Matthias Seitz <matthias.seitz@outlook.de>
2026-07-16 12:27:06 +0200

    fix(ci): restore deterministic example builds

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,51 +16,47 @@ function log () {
 
 # This script will do the following:
 #
-# 1. Gather all the examples from the output of `cargo run --example` command.
+# 1. Gather all example targets from Cargo metadata.
 # 2. Filter out the examples that have external dependencies or are not meant to be run.
 # 3. Pre-build the filtered examples prior to running them.
 # 4. Run all the examples in parallel (up to 10) that are left after filtering.
 function main () {
-    export examples="$(
-        cargo run --example 2>&1 \
-            | grep -E '^ ' \
-            | grep -v \
-            -e 'any_network' \
-            -e 'aws_signer' \
-            -e 'builtin' \
-            -e 'debug_trace_call_many' \
-            -e 'ethereum_wallet' \
-            -e 'foundry_fork_db' \
-            -e 'gcp_signer' \
-            -e 'geth_local_instance' \
-            -e 'ipc' \
-            -e 'keystore_signer' \
-            -e 'ledger_signer' \
-            -e 'permit2_signature_transfer' \
-            -e 'reth_local_instance' \
-            -e 'send_eip7594_transaction' \
-            -e 'subscribe_all_logs' \
-            -e 'subscribe_logs' \
-            -e 'subscribe_pending_transactions' \
-            -e 'trace_call_many' \
-            -e 'trace_call' \
-            -e 'trace_transaction' \
-            -e 'trezor_signer' \
-            -e 'urgent_filler' \
-            -e 'ws_auth' \
-            -e 'ws' \
-            -e 'yubi_signer' \
-            | xargs -n1 echo
-    )"
+    local cargo_args=("$@")
+    local examples=()
+    local metadata
+    metadata="$(cargo metadata "${cargo_args[@]}" --format-version 1 --no-deps)"
 
-    log $GREEN "Building..."
+    while IFS= read -r example; do
+        case "$example" in
+            any_network|aws_signer|builtin|debug_trace_call_many|ethereum_wallet|\
+            foundry_fork_db|gcp_signer|geth_local_instance|ipc|keystore_signer|\
+            ledger_signer|permit2_signature_transfer|reth_local_instance|\
+            send_eip7594_transaction|subscribe_all_logs|subscribe_logs|\
+            subscribe_pending_transactions|trace_call_many|trace_call|\
+            trace_transaction|trezor_signer|urgent_filler|ws_auth|ws|yubi_signer)
+                continue
+                ;;
+        esac
+        examples+=("$example")
+    done < <(
+        printf '%s' "$metadata" \
+            | jq -r '.packages[].targets[] | select(.kind | index("example")) | .name' \
+            | sort -u
+    )
+
+    log "$GREEN" "Building..."
 
     # Pre-build the filtered examples prior to running them.
-    cargo build $(printf -- '--example %s ' $(echo "$examples"))
+    local build_args=()
+    for example in "${examples[@]}"; do
+        build_args+=(--example "$example")
+    done
+    cargo build "${cargo_args[@]}" "${build_args[@]}"
 
-    log $GREEN "Running..."
+    log "$GREEN" "Running..."
 
-    printf '%s\n' $examples | xargs -P4 -I{} bash -c '
+    # shellcheck disable=SC2016
+    printf '%s\n' "${examples[@]}" | xargs -P4 -I{} bash -c '
         name="$1"
         bin="./target/debug/examples/$name"
 
@@ -77,9 +73,9 @@ function main () {
         fi
     ' -- {}
 
-    log $GREEN "Done"
+    log "$GREEN" "Done"
 }
 
 # Run the main function.
 # This prevents partial execution in case of incomplete downloads.
-main
+main "$@"
